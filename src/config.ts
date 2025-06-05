2f72f214826e8271e74dbe5bb7ba4e42dc3f76ec
Gina Beth Russell <ginabethrussell@MacBookPro.cinci.rr.com>
2025-06-05 18:04:14 -0400

    Removes duplicate url segment from base url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const API_BASE =
   process.env.NEXT_PUBLIC_BACKEND_URL ??
   (process.env.NODE_ENV === "production"
-    ? "https://wandersync-backend.onrender.com/api"
-    : "http://localhost:8000/api");
+    ? "https://wandersync-backend.onrender.com"
+    : "http://localhost:8000");
